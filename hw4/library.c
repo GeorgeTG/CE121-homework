c89a5549d88197bc7c67b734a25be9c919375834
GeorgeTG <george_gougoudis@hotmail.com>
2014-05-16 12:16:51 +0300

    Added super nifty debug header

--- hw4/library.c
+++ hw4/library.c
@@ -6,38 +6,40 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#include "debug.h"
 #include "library.h"
 
 int shmid;
 shm_st *memory;
 
 int buf_init(int n) {
-    printf("%u\n", sizeof(shm_st));
+
+    debug("Size of shm_st: %zu\n", sizeof(shm_st));
 
     size_t totalSize = sizeof(shm_st) + n*sizeof(char);
     int retValue = 1;
 
     int _shmid = shmget(SHM_KEY, totalSize, IPC_CREAT | IPC_EXCL | S_IRWXU);
     if( _shmid < 0 ) {
-        printf("ERROR\n");
+        debug("_shmid < 0!\n");
         if( errno == EEXIST ) {
             _shmid = shmget(SHM_KEY, totalSize, 0);
             retValue = 0;
         }
         else {
-            perror("shmget");
+            log_err("shmget");
             return -1;
         }
     }
     printf("%d\n", _shmid);
-    
+
     shmid = _shmid;
     memory = (shm_st*)shmat(shmid, NULL, 0);
     if( memory == (void*)-1 ) {
-        perror("shmat");
+        log_err("shmat");
         return -1;
     }
-    
-    printf("%p\n", memory);
+
+    debug("Memory pointer is: %p\n", memory);
     return retValue;
 }
--- hw4/library.h
+++ hw4/library.h
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <errno.h>
-
 #ifndef SHM_LIBRARY
+
 #define SHM_LIBRARY
 
 #define SHM_KEY 1337
